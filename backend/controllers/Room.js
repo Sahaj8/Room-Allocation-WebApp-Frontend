e521504b9082d638e4220fae817e04a7b975c60c
Sahaj8 <sahaj.vaghasiya8112@gmail.com>
2022-04-26 21:47:10 +0530

    user authentication fixed

--- backend/controllers/Room.js
+++ backend/controllers/Room.js
@@ -14,14 +14,27 @@ export const getRoom = async (req, res) => {
 export const addRoom = (req, res) => {
     // res.send('It works');
     const roomNumber = req.body.roomNumber;
-    const size = req.body.size;
-    const description = req.body.description;
+    // const size = req.body.size;
+    // const description = req.body.description;
 
-    const newRoom = new room({roomNumber,size,description});
+    if(!roomNumber){
+        res.status(400)
+        throw new Error('Please add value')
+    }
+
+    const roomExists = await User.findOne(req.body)
+    console.log(roomExists);
+    if (roomExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    // const newRoom = new room({roomNumber,size,description});
+    const newRoom = new room(req.body);
 
     newRoom.save()
         .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+        .catch(err => res.status(400).json('Error: ' + err.message));
 }
 
 export const deleteRoom = (req, res) => {
--- frontend/src/components/addUser.js
+++ frontend/src/components/addUser.js
@@ -5,6 +5,7 @@ const AddUser = () => {
     const [name, setName] = useState("");
     const [mail, setMail] = useState("");
     const [desc, setDesc] = useState("");
+    let loading=false;
 
     useEffect( ()=>{
         const token = localStorage.getItem('token');
@@ -17,11 +18,16 @@ const AddUser = () => {
                     console.log(res.data.user);
                     if(res.status===201)
                     {  
-                        if(res.data.user.isAdmin === false) window.location="/";
+                        if(res.data.user.isAdmin === false){
+                            window.location.href="/";
+                            alert("Permision denied!")
+                        }
+                        else loading=true;
                     }
                     else
                     {   
-                        alert("Internal Server Error : " + res.status);
+                        alert("Token invalid!");
+                        window.location.href="/login";
                     }
                 })
                 .catch((err) => {
@@ -30,7 +36,7 @@ const AddUser = () => {
                     alert("Internal Server Error");
                 })
         }
-        else  window.location="/login";
+        else  window.location.href="/login";
     },[])
 
     const handleSubmit = async (e) => {
@@ -52,32 +58,39 @@ const AddUser = () => {
 
     return (
         <div className="container">
-            <h2>Add User</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="inputUserName" className="form-label">Username</label>
-                    <textarea 
-                        className="form-control" id="inputUserName" placeholder="Username"
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label className="form-label" htmlFor="inputEmail">Email</label>
-                    <input 
-                        type="email" 
-                        className="form-control" id="inputEmail" placeholder="Email"
-                        onChange={(e) => setMail(e.target.value)}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="inputDescription" className="form-label">Description</label>
-                    <textarea 
-                        className="form-control" id="inputDescription" placeholder="Description"
-                        onChange={(e) => setDesc(e.target.value)}
-                    />
-                </div>
-                <button type="submit" className="btn btn-primary">Add</button>
-            </form>
+            {
+                loading?
+                <>
+                <h2>Add User</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="inputUserName" className="form-label">Username</label>
+                        <textarea 
+                            className="form-control" id="inputUserName" placeholder="Username"
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="inputEmail">Email</label>
+                        <input 
+                            type="email" 
+                            className="form-control" id="inputEmail" placeholder="Email"
+                            onChange={(e) => setMail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="inputDescription" className="form-label">Description</label>
+                        <textarea 
+                            className="form-control" id="inputDescription" placeholder="Description"
+                            onChange={(e) => setDesc(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Add</button>
+                </form>
+                </>
+                :
+                <></>
+            }
         </div>
     );
 }
